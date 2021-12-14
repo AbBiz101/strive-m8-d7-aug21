1a7fc96e390133d71ae075edce9ee6f5c6a36083
Riccardo Gulin <riccardo@nucleode.com>
2021-12-14 10:07:05 +0100

    Adds basic server stuff; adds users

---
+++ src/services/users/index.js
@@ -0,0 +1,43 @@
+import express from "express"
+import UserModel from "./schema.js"
+
+const usersRouter = express.Router()
+
+usersRouter.post("/", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.get("/", async (req, res, next) => {
+  try {
+    const users = await UserModel.find()
+    res.send(users)
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.get("/:id", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.put("/:id", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.delete("/:id", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error)
+  }
+})
+
+export default usersRouter
